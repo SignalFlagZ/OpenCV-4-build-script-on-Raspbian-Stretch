a264ab100b644c94f0ca216f853b912f15f9a25f
SignalFlagZ <Signal0Flag0Z@gmail.com>
2019-03-19 20:13:36 +0900

    test git branch

--- build-OpenCV.sh
+++ build-OpenCV.sh
@@ -4,9 +4,20 @@
 # https://signal-flag-z.blogspot.com/
 # Copyright (c) 2019, Signal Flag "Z"  All rights reserved.
 
+user=`whoami`
+echo 'Your Name :' ${user}
+
 sudo apt update
 sudo apt -y upgrade
 
-user=`whoami`
-echo ${user}
+sudo apt-get install -y build-essential cmake unzip pkg-config
+sudo apt-get install -y libjpeg-dev libpng-dev libtiff-dev
+sudo apt-get install -y libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
+sudo apt-get install -y libxvidcore-dev libx264-dev
+sudo apt-get install -y libgtk-3-dev
+sudo apt-get install -y libcanberra-gtk*
+sudo apt-get install -y libatlas-base-dev gfortran
 
+cd ~/Downloads/
+git clone --depth 1 --branch 4.0.1 https://github.com/opencv/opencv.git
+git clone --depth 1 --branch 4.0.1 https://github.com/opencv/opencv_contrib.git
