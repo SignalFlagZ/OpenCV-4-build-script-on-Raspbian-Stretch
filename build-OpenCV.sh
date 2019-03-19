1217455d438463708c674083ac0449d736e4f0dc
SignalFlagZ <Signal0Flag0Z@gmail.com>
2019-03-19 19:53:24 +0900

    first test

---
+++ build-OpenCV.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Nov. 1 2016
+# Signal Flag "Z"
+# https://signal-flag-z.blogspot.com/
+# Copyright (c) 2019, Signal Flag "Z"  All rights reserved.
+
+sudo apt update
+sudo apt -y upgrade
+
+user = `whoami`
+echo ${user}
+
